13cd4456be832e48426068999718282101491530
zqkang <960623850@qq.com>
2021-03-01 14:12:07 +0800

    version 0.1

---
+++ React Official Website Chinese/02 实践教程/my-app/src/index.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class Square extends React.Component {//zqkang: 方块，即棋盘里的一个个小格
+    render() {
+      return (
+        <button className="square">
+          {/* TODO */}
+        </button>
+      );
+    }
+  }
+  
+  class Board extends React.Component {//zqkang, 板子，即棋盘
+    renderSquare(i) {
+      return <Square />;
+    }
+  
+    render() {
+      const status = 'Next player: X';
+  
+      return (
+        <div>
+          <div className="status">{status}</div>
+          <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+        </div>
+      );
+    }
+  }
+  
+  class Game extends React.Component {//zqkang:游戏本身
+    render() {
+      return (
+        <div className="game">
+          <div className="game-board">
+            <Board />
+          </div>
+          <div className="game-info">
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+          </div>
+        </div>
+      );
+    }
+  }
+  
+  // ========================================
+  
+  ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+  );
+  
